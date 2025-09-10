0165f9521dae632e342eacfa496498c14d32baeb
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-09-10 23:19:05 +0300

    Create couples component and its navigation

---
+++ my-blog/src/app/pages/couples/couples.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-couples',
+  imports: [],
+  templateUrl: './couples.html',
+  standalone:true,
+  styleUrl: './couples.css'
+})
+export class Couples {
+
+}
