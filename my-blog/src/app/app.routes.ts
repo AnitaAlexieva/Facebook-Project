5dc29327d298e4a30bb4a6218396ecf70d373c9c
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-09-13 22:21:04 +0300

    Create about component and its naviagtion

--- my-blog/src/app/app.routes.ts
+++ my-blog/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './pages/home/home';
 import { All } from './pages/all/all';
 import { Couples } from './pages/couples/couples';
 import { Fitness } from './pages/fitness/fitness';
+import { About } from './pages/about/about';
 
 export const routes: Routes = [
     {path:'', component:Home},
     {path:'all', component:All},
     {path:'couples', component:Couples},
-    {path:'fitness', component:Fitness}
+    {path:'fitness', component:Fitness},
+    {path:'about', component:About}
 ];
