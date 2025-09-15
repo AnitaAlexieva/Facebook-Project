3c324e10fffc0770f656ac87602087d05dec52c2
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-09-15 17:12:58 +0300

    Create register component and its navigation

--- my-blog/src/app/app.routes.ts
+++ my-blog/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { All } from './pages/all/all';
 import { Couples } from './pages/couples/couples';
 import { Fitness } from './pages/fitness/fitness';
 import { About } from './pages/about/about';
+import { Register } from './user/register/register';
 
 export const routes: Routes = [
     {path:'', component:Home},
     {path:'all', component:All},
     {path:'couples', component:Couples},
     {path:'fitness', component:Fitness},
-    {path:'about', component:About}
+    {path:'about', component:About},
+    {path:'register', component:Register}
 ];
---
+++ my-blog/src/app/user/register/register.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.html',
+  standalone:true,
+  styleUrl: './register.css'
+})
+export class Register {
+
+}
