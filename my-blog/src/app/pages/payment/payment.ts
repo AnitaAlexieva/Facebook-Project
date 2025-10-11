a9c646d19c02744c6525757b49ac9cb8a3cd02d2
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-10-11 23:14:18 +0300

    Create payment component and its navigation

---
+++ my-blog/src/app/pages/payment/payment.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment',
+  imports: [],
+  standalone:true,
+  templateUrl: './payment.html',
+  styleUrl: './payment.css'
+})
+export class Payment {
+
+}
